8c1b3e5bb44a8e25802fc0eea8cd1fa390f70f80
Gabriel Felipe <gabriel.felipe@correo.ucu.edu.uy>
2024-06-20 15:05:21 -0300

    Agregando documentación a swagger

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 using MySolidWebApi.Interfaces;
 using MySolidWebApi.Services;
 using MySolidWebApi.Models;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+            // Set the comments path for the Swagger JSON and UI.
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+        });
 
 // Register services
 // builder.Services.AddTransient<SistemaPuntuacion, SurfScoreService>();
